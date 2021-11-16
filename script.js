8b51eedd194a0431921aa159fedc2ce324eea497
TobiasBreinhild <tbbr.skp@edu.mercantec.dk>
2021-11-16 14:49:49 +0100

    Version 1.7

--- script.js
+++ script.js
@@ -16,13 +16,18 @@ function WriteInCanvas(){
     var x = document.getElementById("demo");
     function getLocation() {
         if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(showPosition);
+            navigator.geolocation.getCurrentPosition(showPositions);
         } else {
             x.innerHTML = "Geolocation is not supported by this browser.";
         }
     }
-    
+    /*
     function showPosition(position) {
+        x.innerHTML = "Latitude: " + position.coords.latitude +
+                 "<br>Longitude: " + position.coords.longitude;
+    }
+*/
+    function showPositions(position) {
         x.fillText("Latitude: " + position.coords.latitude +
                  "<br>Longitude: " + position.coords.longitude);
     }
