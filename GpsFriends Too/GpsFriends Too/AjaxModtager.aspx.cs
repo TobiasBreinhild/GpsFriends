3aeb2ba8e856182753b31211bd1e08fc2b14df0a
TobiasBreinhild <tbbr.skp@edu.mercantec.dk>
2021-11-18 11:16:33 +0100

    Something

---
+++ GpsFriends Too/GpsFriends Too/AjaxModtager.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class AjaxModtager : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+    }
+    [WebMethod]
+    public static string OnSubmit(string name, bool isGoing, string returnAddress)
+    {
+        return "it worked" + name;
+    }
+}
