69f04a87ee443ec3f12ff58a3331023908e1e6d9
TobiasBreinhild <tbbr.skp@edu.mercantec.dk>
2021-11-23 10:24:32 +0100

    Added working project made in visual studio 2019
    Visual studio code didn't work

---
+++ GpsFriendsAgainAgain/AjaxModtager.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace GpsFriendsAgainAgain
+{
+    public partial class AjaxModtager : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        [WebMethod]
+        public static string OnSubmit(string name, bool isGoing, string returnAddress)
+        {
+            return name + ", " + returnAddress + " and is going: " + isGoing;
+        }
+
+    }
+}
